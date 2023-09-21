97a50dc31272b551018758b07e996531d0aa1c5a
kitkatyourmom <sarahlridings@gmail.com>
2023-09-20 21:42:26 -0600

    add component folder with component elements

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar/Navbar";
+import Welcome from "./components/Welcome/Welcome";
+import Signup from "./components/Signup/Signup";
+import Login from "./components/Login/Login";
+import Form from "./components/Form/Form";
+import Data from "./components/Data/Data";
+import SavedSearch from "./components/SavedSearch/SavedSearch";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <Navbar/>
+          <Welcome/>
+          <Signup/>
+          <Login/>
+          <Form/>
+          <Data/>
+          <SavedSearch/>
+          <Footer/>
+      </div>
   );
 }
 
