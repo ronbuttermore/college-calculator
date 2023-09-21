b24a3efaf0cbb95a4bc807dcd5b938f60a3c8541
Ron Buttermore <ron.buttermore@gmail.com>
2023-09-21 14:14:56 -0600

    added user auth for signup

--- client/src/pages/Signup/Signup.js
+++ client/src/pages/Signup/Signup.js
@@ -1,16 +1,94 @@
-import React from 'react';
-import './Signup.css';
-import SignupForm from '../../components/Forms/SignupForm';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-function Signup() {
-    return (
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../../utils/mutations';
+
+import Auth from '../../utils/auth';
+
+const Signup = () => {
+  const [formState, setFormState] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
+  const [addUser, { error, data }] = useMutation(ADD_USER);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    console.log(formState);
+
+    try {
+      const { data } = await addUser({
+        variables: { ...formState },
+      });
+
+      Auth.login(data.addUser.token);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return (
+    <div>
+        <h4>Sign Up</h4>
         <div>
-            <h2>This will be the signup page</h2>
-            <SignupForm />
-            <p>button here to submit and continue to results page</p>
-            <button>Submit</button>
+        {data ? (
+            <p>
+            Success! You may now head{' '}
+            <Link to="/">back to the homepage.</Link>
+            </p>
+        ) : (
+            <form onSubmit={handleFormSubmit}>
+            <input
+                className="form-input"
+                placeholder="Your username"
+                name="username"
+                type="text"
+                value={formState.name}
+                onChange={handleChange}
+            />
+            <input
+                className="form-input"
+                placeholder="Your email"
+                name="email"
+                type="email"
+                value={formState.email}
+                onChange={handleChange}
+            />
+            <input
+                className="form-input"
+                placeholder="******"
+                name="password"
+                type="password"
+                value={formState.password}
+                onChange={handleChange}
+            />
+            <button
+                type="submit"
+            >
+                Submit
+            </button>
+            </form>
+        )}
+
+        {error && (
+            <div>
+            {error.message}
+            </div>
+        )}
         </div>
-    );
+    </div>
+  );
 };
 
 export default Signup;
