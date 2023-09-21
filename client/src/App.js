17b289555c5200cec98236eb8c50874394675668
kitkatyourmom <sarahlridings@gmail.com>
2023-09-20 21:45:08 -0600

    updated links in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import Navbar from "./components/Navbar/Navbar";
-import Welcome from "./components/Welcome/Welcome";
-import Signup from "./components/Signup/Signup";
-import Login from "./components/Login/Login";
-import Form from "./components/Form/Form";
-import Data from "./components/Data/Data";
+import Welcome from "./components/Welcome/welcome";
+import Signup from "./components/Signup/signup";
+import Login from "./components/Login/login";
+import Form from "./components/Form/form";
+import Data from "./components/Data/data";
 import SavedSearch from "./components/SavedSearch/SavedSearch";
-import Footer from "./components/Footer/Footer";
+import Footer from "./components/Footer/footer";
 
 function App() {
   return (
