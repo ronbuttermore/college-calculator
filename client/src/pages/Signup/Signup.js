2549f397ad0bd9a8d2c93bfd5646187421d03e19
kitkatyourmom <sarahlridings@gmail.com>
2023-09-23 12:35:44 -0600

    add useRef and useState variables in Signup

--- client/src/pages/Signup/Signup.js
+++ client/src/pages/Signup/Signup.js
@@ -12,9 +12,27 @@ import Auth from '../../utils/auth';
 
 const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
-const REGISTER_URL = '/register';
+const REGISTER_URL = '/signup';
 
 const Signup = () => {
+  const userRef = useRef();
+  const errRef = useRef();
+
+  const [user, setUser] = useState('');
+  const [validName, setValidName] = useState(false);
+  const [userFocus, setUserFocus] = useState(false);
+
+  const [pwd, setPwd] = useState('');
+  const [validPwd, setValidPwd] = useState(false);
+  const [pwdFocus, setPwdFocus] = useState(false);
+
+  const [matchPwd, setMatchPwd] = useState('');
+  const [validMatch, setValidMatch] = useState(false);
+  const [matchFocus, setMatchFocus] = useState(false);
+
+  const [errMsg, setErrMsg] = useState('');
+  const [success, setSuccess] = useState(false);
+
   const [formState, setFormState] = useState({
     username: '',
     email: '',
