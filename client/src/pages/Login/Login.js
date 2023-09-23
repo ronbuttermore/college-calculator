7009c594da87408d3726071fddd777f38a1d3848
kitkatyourmom <sarahlridings@gmail.com>
2023-09-23 12:15:12 -0600

    add return information in login.js

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -2,7 +2,6 @@ import React, { useRef, useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { faCheck, faTimes, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import axios from "./api/axios";
 
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../../utils/mutations';
@@ -76,8 +75,20 @@ const Login = (props) => {
       });
 
       Auth.login(data.login.token);
-    } catch (e) {
-      console.error(e);
+      setSuccess(true);
+      // clear input fields
+      setUser('');
+      setPwd('');
+      setMatchPwd('');
+    } catch(err) {
+      if (!err?.response) {
+          setErrMsg ('No Server Response');
+      } else if (err.response.status === 409) {
+          setErrMsg('Username Taken')
+      } else {
+          setErrMsg('Registration Failed')
+      }
+      errRef.current.focus();
     }
 
     setFormState({
@@ -87,47 +98,140 @@ const Login = (props) => {
   };
 
   return (
-    <main id='login'>
-        <h4 className='loginTitle'>Login</h4>
-        <div className='loginSection'>
-        {data ? (
-            <p>
-            Success! You may now head{' '}
-            <Link to="/">back to the homepage.</Link>
+    <>
+    {success ? (
+      <section>
+        <h1>Success!</h1>
+          <p>
+            <a href="#">Sign In</a>
             </p>
-        ) : (
-            <form className='form' onSubmit={handleFormSubmit}>
+      </section>
+            ) : (
+      <section id='login'>
+        <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
+        <h1>Login</h1>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="username">
+            Username:
+            <FontAwesomeIcon icon={faCheck} className={validName ? "valid" : "hide"} />
+            <FontAwesomeIcon icon={faTimes} className={validName || !user ? "hide" : "invalid"} />
+          </label>
             <input
-                className="form-input"
-                placeholder="Your email"
-                name="email"
-                type="email"
-                value={formState.email}
-                onChange={handleChange}
+              type="text"
+              id="username"
+              ref={userRef}
+              autoComplete="off"
+              onChange={(e) => setUser(e.target.value)}
+              value={user}
+              required
+              aria-invalid={validName ? "false" : "true"}
+              aria-describedby="uidnote"
+              onFocus={() => setUserFocus(true)}
+              onBlur={() => setUserFocus(false)}
             />
-            <input
-                className="form-input"
-                placeholder="******"
-                name="password"
-                type="password"
-                value={formState.password}
-                onChange={handleChange}
-            />
-            <button
-                type="submit"
-            >
-                Submit
-            </button>
-            </form>
-        )}
+            <p id="uidnote" className={userFocus && user && !validName ? "instructions" : "offscreen"}>
+              <FontAwesomeIcon icon={faInfoCircle} />
+              4 to 24 characters.<br />
+              Must begin with a letter.<br />
+              Letters, numbers, underscores, hyphens allowed.
+            </p>
 
-        {error && (
-            <div>
-            {error.message}
-            </div>
-        )}
-        </div>
-    </main>
+
+          <label htmlFor="password">
+              Password:
+              <FontAwesomeIcon icon={faCheck} className={validPwd ? "valid" : "hide"} />
+              <FontAwesomeIcon icon={faTimes} className={validPwd || !pwd ? "hide" : "invalid"} />
+          </label>
+          <input
+            type="password"
+            id="password"
+            onChange={(e) => setPwd(e.target.value)}
+            value={pwd}
+            required
+            aria-invalid={validPwd ? "false" : "true"}
+            aria-describedby="pwdnote"
+            onFocus={() => setPwdFocus(true)}
+            onBlur={() => setPwdFocus(false)}
+          />
+          <p id="pwdnote" className={pwdFocus && !validPwd ? "instructions" : "offscreen"}>
+            <FontAwesomeIcon icon={faInfoCircle} />
+            8 to 24 characters.<br />
+            Must include uppercase and lowercase letters, a number and a special character.<br />
+            Allowed special characters: <span aria-label="exclamation mark">!</span> <span aria-label="at symbol">@</span> <span aria-label="hashtag">#</span> <span aria-label="dollar sign">$</span> <span aria-label="percent">%</span>
+          </p>
+
+
+          <label htmlFor="confirm_pwd">
+            Confirm Password:
+            <FontAwesomeIcon icon={faCheck} className={validMatch && matchPwd ? "valid" : "hide"} />
+            <FontAwesomeIcon icon={faTimes} className={validMatch || !matchPwd ? "hide" : "invalid"} />
+          </label>
+          <input
+            type="password"
+            id="confirm_pwd"
+            onChange={(e) => setMatchPwd(e.target.value)}
+            value={matchPwd}
+            required
+            aria-invalid={validMatch ? "false" : "true"}
+            aria-describedby="confirmnote"
+            onFocus={() => setMatchFocus(true)}
+            onBlur={() => setMatchFocus(false)}
+          />
+          <p id="confirmnote" className={matchFocus && !validMatch ? "instructions" : "offscreen"}>
+            <FontAwesomeIcon icon={faInfoCircle} />
+            Must match the first password input field.
+          </p>
+
+          <button disabled={!validName || !validPwd || !validMatch ? true : false}>Sign Up</button>
+        </form>
+          <p>
+            Already registered?<br />
+            <span className="line">
+            {/*put router link here*/}
+            <a href="#">Sign In</a>
+            </span>
+          </p>
+          {/* <div className='loginSection'>
+          {data ? (
+              <p>
+              Success! You may now head{' '}
+              <Link to="/">back to the homepage.</Link>
+              </p>
+          ) : (
+              <form className='form' onSubmit={handleFormSubmit}>
+              <input
+                  className="form-input"
+                  placeholder="Your email"
+                  name="email"
+                  type="email"
+                  value={formState.email}
+                  onChange={handleChange}
+              />
+              <input
+                  className="form-input"
+                  placeholder="******"
+                  name="password"
+                  type="password"
+                  value={formState.password}
+                  onChange={handleChange}
+              />
+              <button
+                  type="submit"
+              >
+                  Submit
+              </button>
+              </form>
+          )}
+
+          {error && (
+              <div>
+              {error.message}
+              </div>
+          )}
+          </div> */}
+      </section>
+    )}
+    </>
   );
 };
 
