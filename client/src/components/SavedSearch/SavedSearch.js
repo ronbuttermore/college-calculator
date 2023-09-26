66a27e9865e17fafc25adc1ea5c22c1e51dc8171
kitkatyourmom <sarahlridings@gmail.com>
2023-09-25 20:02:24 -0600

    add checkbox for saved searches

--- client/src/components/SavedSearch/SavedSearch.js
+++ client/src/components/SavedSearch/SavedSearch.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import './SavedSearch.css';
 
 function SavedSearch() {
+    const [checked, setChecked] = React.useState(false);
+    function handleChange(e) {
+        setChecked(e.target.checked);
+     }
     return (
-        <div id='savedSearches'>
-            <p id='placeholder'>Here's where the user's saved searches will show up</p>
+        <div id='saved-searches'>
+            <h2 className='saved-search-section-tile'>Saved Searches</h2>
+            <div className='saved-search-container'>
+                <div className='saved-search-box'>
+                    <div className='saved-box-title'>
+                        <h4 className='saved-search-title'>University Name</h4>
+                        <input value = "test" type = "checkbox" onChange = {handleChange} />
+                        <br></br>
+                        {checked ? (
+                            <div>
+                                {/* Checkbox is checked.  */}
+                            </div>
+                        ) : (
+                            <div>
+                                {/* Checkbox is not checked. */}
+                            </div>
+                        )}
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
