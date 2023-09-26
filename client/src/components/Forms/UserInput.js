a1805bf3f120d3665c3b463a571a75877d02f42a
kitkatyourmom <sarahlridings@gmail.com>
2023-09-25 21:02:19 -0600

    update syling in forms on UserInput.js

--- client/src/components/Forms/UserInput.js
+++ client/src/components/Forms/UserInput.js
@@ -6,6 +6,15 @@ import { QUERY_SEARCHES } from '../../utils/queries';
 
 const UserInput = () => {
 
+    const userFormStyle = {
+        fontSize: "24px",
+        padding: "0.5rem",
+        borderRadius: "0.5rem",
+        border: "2px solid #BABABA",
+        backgroundColor: "white",
+        margin: "1rem 0",
+      };
+
     const [submitState, setSubmitState] = useState({
         university: "",
         degree: "",
@@ -37,23 +46,23 @@ const UserInput = () => {
         <div id='inputsearch'>
             <form onSubmit={loadData}>
                 <h3 className='form-name'>University</h3>
-                <input className='form-input-box' type="text" name="university" onChange={handleChange}></input>
+                <input style={userFormStyle} type="text" name="university" onChange={handleChange}></input>
                 <h3 className='form-name'>Degree</h3>
-                <textarea className='form-input-box' type="text" name="degree" onChange={handleChange}></textarea>
+                <textarea style={userFormStyle} type="text" name="degree" onChange={handleChange}></textarea>
                 <h3 className='form-name'>No. Years</h3>
-                <input className='form-input-box' type="text" name="noyears" onChange={handleChange} pattern= "[0-9]*"></input>
+                <input style={userFormStyle} type="text" name="noyears" onChange={handleChange} pattern= "[0-9]*"></input>
                 <h3 className='form-name'>Tuition</h3>
-                <input className='form-input-box' type="text" name="tuition" onChange={handleChange} pattern= "[0-9]*"></input>
+                <input style={userFormStyle} type="text" name="tuition" onChange={handleChange} pattern= "[0-9]*"></input>
                 <h3 className='form-name'>Scholarships</h3>
-                <input className='form-input-box' type="text" name="scholarships" onChange={handleChange} pattern= "[0-9]*"></input>
+                <input style={userFormStyle} type="text" name="scholarships" onChange={handleChange} pattern= "[0-9]*"></input>
                 <h3 className='form-name'>Loan Amount</h3>
-                <input className='form-input-box' type="text" name="loanAmount" onChange={handleChange} pattern= "[0-9]*"></input>
+                <input style={userFormStyle} type="text" name="loanAmount" onChange={handleChange} pattern= "[0-9]*"></input>
                 <h3 className='form-name'>Loan Interest</h3>
-                <input className='form-input-box' type="text" name="loanInterest" onChange={handleChange} pattern= "[0-9]*"></input>
+                <input style={userFormStyle} type="text" name="loanInterest" onChange={handleChange} pattern= "[0-9]*"></input>
                 <h3 className='form-name'>Loan Term</h3>
-                <input className='form-input-box' type="text" name="loanTerm" onChange={handleChange} pattern= "[0-9]*"></input>
+                <input style={userFormStyle} type="text" name="loanTerm" onChange={handleChange} pattern= "[0-9]*"></input>
                 <h3 className='form-name'>Projected Salary</h3>
-                <input className='form-input-box' type="text" name="projectedSalary" onChange={handleChange} pattern= "[0-9]*"></input>
+                <input style={userFormStyle} type="text" name="projectedSalary" onChange={handleChange} pattern= "[0-9]*"></input>
             </form>
             <br />
             <button id='inputsearchbtn' onClick={loadData} >Show Results</button>
