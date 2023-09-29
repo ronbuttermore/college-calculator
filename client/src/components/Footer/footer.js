f5fb7465686f471effa01f97f09777f5d3e153a0
kitkatyourmom <sarahlridings@gmail.com>
2023-09-28 20:00:58 -0600

    add mobile responsiveness to footer and navbar

--- client/src/components/Footer/footer.js
+++ client/src/components/Footer/footer.js
@@ -4,7 +4,7 @@ import './footer.css';
 const Footer = () => {
     return (
         <footer className="footer">
-            Made by &nbsp;<a href="https://github.com/DanielFord11" target="_blank"> Daniel Ford</a> , &nbsp;<a href="https://github.com/renrojas" target="_blank"> Ren Rojas</a> , &nbsp;<a href="https://github.com/ronbuttermore" target="_blank"> Ron Buttermore</a> , and &nbsp;<a href="https://github.com/Sarah-Paterson" target="_blank"> Sarah Paterson</a>
+            <p>Made by &nbsp;<a href="https://github.com/DanielFord11" target="_blank"> Daniel Ford</a> , &nbsp;<a href="https://github.com/renrojas" target="_blank"> Ren Rojas</a> , <br id="footer-break"/>&nbsp;<a href="https://github.com/ronbuttermore" target="_blank"> Ron Buttermore</a> , and &nbsp;<a href="https://github.com/Sarah-Paterson" target="_blank"> Sarah Paterson</a></p>
         </footer>
     );
 };
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 import { Link } from 'react-router-dom';
 import Auth from '../../utils/auth';
 
 import logo from "../../assets/logo.png"
+import menu from "../../assets/menu.png"
 
 function Navbar() {
     const logout = (event) => {
         event.preventDefault();
         Auth.logout();
       };
+      const [showMenu, setShowMenu] = useState(false)
       return (
         <div className="navbar">
             <div>
@@ -20,14 +22,21 @@ function Navbar() {
             <div>
               {Auth.loggedIn() ? (
                 <>
-                  <button className="nav-btn" onClick={() => {
-                    document.getElementById('saved-searches').scrollIntoView({behavior: 'smooth'});
-                    }}>
-                    Saved Searches
-                  </button>
-                  <button className="logout-btn" onClick={logout}>
-                    Logout
-                  </button>
+                  <div className="desktopMenu">
+                    <button className="nav-btn" onClick={() => {
+                      document.getElementById('saved-searches').scrollIntoView({behavior: 'smooth'});
+                      }}>
+                      Saved Searches
+                    </button>
+                    <button className="logout-btn" onClick={logout}>
+                      Logout
+                    </button>
+                  </div>
+                  <img src={menu} alt="menu" className="mobMenu" onClick={() =>setShowMenu(!showMenu)}/>
+                  <div className="navMenu" style={{display: showMenu? 'flex': 'none'}}>
+                    <Link to='saved-searches' activeClass='active' spy={true} smooth={true} offset={-50} duration={500} className="listItem" onClick={() =>setShowMenu(false)} >Saved Searches</Link>
+                    <Link activeClass='active' className="listItem" onClick={logout} >Logout</Link>
+                  </div>
                 </>
               ) : (
                 <>
