ade21e22f1c6437366d4a2b8cd00ba74719f495b
Ron Buttermore <ron.buttermore@gmail.com>
2023-09-30 10:59:38 -0600

    added search filtering by user

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -4,24 +4,33 @@ import Auth from '../../utils/auth';
 import Results from '../Results/Results'
 import Welcome from '../../components/Welcome/Welcome';
 import SavedSearch from '../../components/SavedSearch/SavedSearch';
+import { useQuery } from '@apollo/client';
+import { QUERY_SEARCHES } from '../../utils/queries';
 
 function Home() {
-    return (
-        <div>
-            <div>
-              {Auth.loggedIn() ? (
-                <>
-                  <Results />
-                  <SavedSearch />
-                </>
-              ) : (
-                <>
-                  <Welcome />
-                </>
-              )}
-            </div>
-        </div>
-    );
+
+  const { loading, data } = useQuery(QUERY_SEARCHES);
+
+  const searches = data?.searches || [];
+
+  return (
+      <div>
+          <div>
+            {Auth.loggedIn() ? (
+              <>
+                <Results />
+                <SavedSearch 
+                  searches={searches}
+                />
+              </>
+            ) : (
+              <>
+                <Welcome />
+              </>
+            )}
+          </div>
+      </div>
+  );
 };
 
 export default Home;
