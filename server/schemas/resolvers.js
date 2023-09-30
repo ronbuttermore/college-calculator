1bfbf7f34dd2459867c9cacb1b56b302f58d5f9e
Ron Buttermore <ron.buttermore@gmail.com>
2023-09-30 06:26:07 -0600

    added mutations for user auth and adding and removing searches

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,6 @@
+const { AuthenticationError } = require('apollo-server-express');
 const { User, Search } = require('../models');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
@@ -8,12 +10,54 @@ const resolvers = {
     user: async (parent, args) => {
       return await User.findById(args.id).populate('searches');
     },
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id }).populate('searches');
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
     searches: async () => {
       return await Search.find({});
     },
     search: async (parent, args) => {
       return await Search.findById(args.id);
     }
+  },
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new AuthenticationError('No user found with this email address');
+      }
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    addSearch: async (parent,  {searchData} , context) => {
+      if (context.user) {      
+        const user = await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $push: { addSearch: searchData } },
+          {new: true}
+        );
+        return user;    
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+
+    removeSearch: async (parent, { searchId }) => {
+      return Search.findOneAndDelete({ _id: searchId });
+    },
   }
 };
 
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -20,11 +20,36 @@ const typeDefs = gql`
     stateTaxPercentage: Int
   }
 
+  input SearchInput {
+    _id: ID
+    university: String
+    major: String
+    loanAmount: Int
+    interestRate: Int
+    loanTerm: Int
+    annualSalary: Int
+    stateTaxPercentage: Int
+  }
+
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
   type Query {
     users: [User]
     searches: [Search]
     user(id: ID!): User
     search(id: ID!): Search
+    me: User
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    addSearch(searchData: SearchInput!): User
+    removeSearch(searchId: ID!): Search
   }
 `;
 
