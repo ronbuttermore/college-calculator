478f70b86791ada18b603c5e51222badf2b89092
Ron Buttermore <ron.buttermore@gmail.com>
2023-09-30 14:05:36 -0600

    moved saved searches into result to avoid auth error

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -3,20 +3,8 @@ import './Home.css';
 import Auth from '../../utils/auth';
 import Results from '../Results/Results'
 import Welcome from '../../components/Welcome/Welcome';
-import SavedSearch from '../../components/SavedSearch/SavedSearch';
-import { useQuery } from '@apollo/client';
-import { QUERY_SEARCHES } from '../../utils/queries';
 
 function Home() {
-  const currentUser = Auth.getProfile().data.username;
-
-  console.log(currentUser);
-
-  const { loading, data } = useQuery(QUERY_SEARCHES, { variables: { searchedBy: currentUser }, });
-
-  console.log(data);
-
-  const searches = data?.searches || [];
 
   return (
       <div>
@@ -24,15 +12,6 @@ function Home() {
             {Auth.loggedIn() ? (
               <>
                 <Results />
-                <div>
-                {loading ? (
-                  <div>Loading...</div>
-                ) : (
-                  <SavedSearch
-                  searches={searches}
-                />
-          )}
-          </div>
               </>
             ) : (
               <>
